5b0ee0788dd1114992e4e494d1feda44efd55dff
Juan Antonio <juagisla@gmail.com>
2020-02-13 17:50:11 +0100

    Starting Theheader

---
+++ src/components/Theheader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>HOLIS</h1>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Theheader',
+        computed: {
+
+        }
+    }
+</script>
